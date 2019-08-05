6f1367365425b4f954500c70f7aa03fad9763a80
vladimirov <denis.vladimiroff@gmail.com>
2019-08-05 18:51:39 +0300

    car exercise updated

--- car.py
+++ car.py
@@ -1,10 +1,19 @@
 command = ""
+started = False
 while True:
-    command = input("").lower()
+    command = input("> ").lower()
     if command == 'start':
-        print('Car started! Ready to go!')
+        if started:
+            print('Car is already started')
+        else:
+            started = True
+            print('Car started! Ready to go!')
     elif command == 'stop':
-        print('Car stopped.')
+        if not started:
+            print('Car is not started')
+        else:
+            started = False
+            print('Car stopped')
     elif command == 'help':
         print("""
         start - to start the car
