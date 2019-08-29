5a490ba94b447b6f5f8abeb93f5a1251f51fb727
vladimirov <denis.vladimiroff@gmail.com>
2019-08-29 16:48:42 +0300

    exceptions

---
+++ inheritance.py
@@ -0,0 +1,17 @@
+class Mammal:
+    def walk(self):
+        print("Walk")
+
+class Dog(Mammal):
+    def bark(self):
+        print("Bark!")
+
+class Cat(Mammal):
+    def be_annoying(self):
+        print("Annoing")
+
+dog = Dog()
+dog.walk()
+
+cat = Cat()
+cat.be_annoying()
